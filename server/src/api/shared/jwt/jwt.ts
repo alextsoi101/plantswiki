bdd7d29f4920bb4d1e24845b4fc2c3b56d4ca30b
Alex Tsoi <alextsoi101@gmail.com>
2023-10-30 21:13:21 -0500

    Add verifyJwt function

--- server/src/api/shared/jwt/jwt.ts
+++ server/src/api/shared/jwt/jwt.ts
@@ -1,7 +1,13 @@
 import jwt from 'jsonwebtoken';
 
+const SECRET_KEY = 'secret_some_key132';
+
 export const generateJwt = (id: number, email: string, role: string): string => {
   return jwt.sign(
-    {id, email, role}, process.env.SECRET_KEY, {expiresIn: '24h'}
+    {id, email, role}, SECRET_KEY, {expiresIn: '24h'}
   )
+}
+
+export const verifyJwt = (token: string): jwt.JwtPayload | string => {
+  return jwt.verify(token, SECRET_KEY);
 }
