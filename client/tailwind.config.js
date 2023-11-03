98fc309ef7f75014aa8716dad2b0dff6279dcb38
Alex Tsoi <alextsoi101@gmail.com>
2023-11-02 22:25:52 -0500

    Setup tailwind

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,7 +1,42 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
+    colors: {
+      'white': '#ffffff',
+      'blue': '#008285',
+      'dark': '#262626',
+      'gray': '#757575',
+      'silver': '#C8C8C8',
+      'gray-light': '#f4f3f1',
+    },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    fontSize: {
+      sm: '0.875rem',
+      base: '1rem',
+      xl: '1.25rem',
+      '2xl': '1.563rem',
+      '3xl': '1.953rem',
+      '4xl': '2.441rem',
+      '5xl': '3.052rem',
+    },
+    fontWeight: {
+      thin: '100',
+      extralight: '200',
+      light: '300',
+      normal: '400',
+      medium: '500',
+      semibold: '600',
+      bold: '700',
+      extrabold: '800',
+      black: '900',
+    },
     extend: {},
   },
   plugins: [],
