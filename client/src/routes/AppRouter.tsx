159525d3803045f458839c6f59011b3d179046e1
Alex Tsoi <alextsoi101@gmail.com>
2023-11-03 17:47:40 -0500

    Update AppRouter

--- client/src/routes/AppRouter.tsx
+++ client/src/routes/AppRouter.tsx
@@ -1,5 +1,6 @@
-import {Routes, Route, Navigate, Outlet} from 'react-router-dom';
+import {Routes, Route, Outlet} from 'react-router-dom';
 import Header from '../components/layout/Header/Header';
+import Footer from '../components/layout/Footer/Footer';
 import PublicRouter from './PublicRouter';
 
 const AppRouter = () => {
@@ -11,7 +12,7 @@ const AppRouter = () => {
           <>
             <Header />
             <Outlet />
-            {/* <Footer /> */}
+            <Footer />
           </>
         }
       >
