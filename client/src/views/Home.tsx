bb38049ab7afa22c10f42b6ceb62969024b1fef9
Alex Tsoi <alextsoi101@gmail.com>
2023-11-04 01:02:52 -0500

    Add Home page

---
+++ client/src/views/Home.tsx
@@ -0,0 +1,29 @@
+import { FC } from "react";
+import SearchForm from "../features/search/components/SearchForm";
+
+const Home: FC = () => {
+  return (
+    <div>
+      <div className="bg-plant bg-cover bg-center h-[600px]">
+        <div className="flex justify-center items-center bg-black/70 w-[100%] h-[100%]">
+          <div className="flex flex-col justify-center items-center w-[800px]">
+            <h1 className="text-white text-4xl font-light drop-shadow-xl">
+              Welcome to Plants of the World
+            </h1>
+            <div className="text-white text-xl font-extralight drop-shadow-xl">
+              Browse more than 1,424,000 global plants
+            </div>
+            <div className="w-[720px] mt-[50px] shadow-xl">
+              <SearchForm />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div>
+
+      </div>
+    </div>
+  )
+}
+
+export default Home;
