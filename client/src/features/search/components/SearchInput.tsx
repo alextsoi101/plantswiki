4ba19a38b96becc9d9eccddf5f60adb1753d1a32
Alex Tsoi <alextsoi101@gmail.com>
2023-11-04 01:02:03 -0500

    Edit search input UI

--- client/src/features/search/components/SearchInput.tsx
+++ client/src/features/search/components/SearchInput.tsx
@@ -1,15 +1,22 @@
-
-
 const SearchInput = () => {
+
   return (
-    <div className="flex items-center w-[100%] h-[50px] rounded-full">
+    <div className="flex items-center w-[100%] rounded-full">
       <input 
         type="text" 
-        className="w-[100%] h-[100%] rounded-l-full"
+        placeholder="Search..."
+        className="w-[100%] h-[50px] rounded-l-full px-[26px] text-dark-black placeholder:text-dark"
       />
       <div className="min-w-[50px]">
-        <button className="flex items-center justify-center w-[100%] h-[50px] rounded-r-full">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+        <button className="flex items-center justify-center w-[100%] h-[50px] rounded-r-full bg-white duration-200 hover:text-blue">
+          <svg 
+            xmlns="http://www.w3.org/2000/svg" 
+            fill="none" 
+            viewBox="0 0 24 24" 
+            strokeWidth={1.5} 
+            stroke="currentColor" 
+            className="w-6 h-6"
+          >
             <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
           </svg>
         </button>
