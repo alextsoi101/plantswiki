af66e43c4add7e6bd57ae0e284722875c8eec164
Alex Tsoi <alextsoi101@gmail.com>
2023-11-05 00:58:44 -0500

    Header responsive design

--- client/src/components/layout/Header/Header.tsx
+++ client/src/components/layout/Header/Header.tsx
@@ -9,7 +9,10 @@ const Header: FC = () => {
   const [buttonHover, setButtonHover] = useState(false);
 
   return (
-    <header className="bg-white py-[9px] px-[20px]">
+    <header className="bg-white
+      xs:px-[10px] xs:py-[4px]
+      md:px-[20px] md:py-[9px]"
+    >
       <div className="flex justify-between align-center">
         <div className="flex items-center">
           <div>
@@ -24,16 +27,27 @@ const Header: FC = () => {
               />
             </button>
           </div>
-          <div className="ml-[12px]">
+          <div className="xs:ml-[5px] md:ml-[12px]">
             <img 
-              className="h-[50px]"
+              className="xs:h-[30px] md:h-[50px]"
               src={logo} 
               alt="logo" 
             />
           </div>
         </div>
-        <div className="flex content-center">
+        <div className="content-center 
+          xs:hidden md:flex"
+        >
           <NavBar />
+        </div>
+        <div className="items-center
+          xs:flex md:hidden"
+        >
+          <button className="flex content-center">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+            </svg>
+          </button>
         </div>
       </div>
     </header>
