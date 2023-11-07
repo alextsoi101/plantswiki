7cdb67a2b566d65d67377d6e7c72ee1035744560
Alex Tsoi <alextsoi101@gmail.com>
2023-11-06 23:16:50 -0600

    Update public router

--- client/src/routes/PublicRouter.tsx
+++ client/src/routes/PublicRouter.tsx
@@ -1,11 +1,13 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
 import Home from '../views/Home';
+import Plants from '../views/Plants';
 
 const PublicRouter = () => {
 
   return (
     <Routes>
-      <Route path='/' element={<Home/>} />
+      <Route path='/' element={<Home />} />
+      <Route path='/plants' element={<Plants />} />
       <Route
         path='*'
         element={<Navigate to='/' replace />}
