9792fbfa5a8fe17db8733b4b7f2d226be2ee60c7
Alex Tsoi <alextsoi101@gmail.com>
2023-11-06 22:54:25 -0600

    Edit PlantsRowCard

--- client/src/features/search/components/SearchResults/PlantRowCard.tsx
+++ client/src/features/search/components/SearchResults/PlantRowCard.tsx
@@ -1,15 +1,44 @@
 import { FC } from "react";
+import plantblueicon from '../../../../assets/icons/plant-blue.svg';
+import plantgrayicon from '../../../../assets/icons/plant-gray.svg';
 
 interface PlantRowCardProps {
-  results: [];
-  total: number;
-
+  id: number;
+  rank: string;
+  name: string;
+  author: string;
+  imageUrl?: string | null;
 }
 
-const PlantRowCard: FC = () => {
+const PlantRowCard: FC<PlantRowCardProps> = ({id, rank, name, author, imageUrl}) => {
   return (
-    <div>
-      
+    <div className={`flex items-center p-[30px] bg-white cursor-pointer duration-150 h-full hover:ring-[10px] ring-inset 
+      ${imageUrl ? 'ring-[#40a1a4]' : 'ring-[#989898]' }`}
+    >
+      <div className="text-sm font-bold xs:w-[100px] md:w-[180px]">
+        {imageUrl ?
+          <div className="flex items-center text-blue">
+            <img src={plantblueicon} alt="plant-icon" />
+            <span className="ml-1">{rank.toUpperCase()}</span>
+          </div>
+          :
+          <div className="flex items-center text-gray">
+            <img src={plantgrayicon} alt="plant-icon" />
+            <span className="ml-1">{rank.toUpperCase()}</span>
+          </div>
+        }
+      </div>
+      <div className="flex 
+        xs:flex-col xs:justify-start xs:items-start
+        md:flex-row xs:justify-normal md:items-center"
+      >
+        <div className="text-[18px] font-medium text-dark-black italic">
+          {name}
+        </div>
+        <div className="text-base font-light text-dark xs:ml-[0px] md:ml-[10px]">
+          {author}
+        </div>
+      </div>
     </div>
   )
 }
