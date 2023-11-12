ddcaf0be29ed8bb36743a1069dfac0806a331160
Alex Tsoi <alextsoi101@gmail.com>
2023-11-12 15:32:29 -0600

    Edit blog main page

--- client/src/views/Blog.tsx
+++ client/src/views/Blog.tsx
@@ -1,15 +1,19 @@
 import { FC } from 'react';
+import TopicHighlightList from '../features/blog/components/Topics/TopicHighlightList';
+import ArticeList from '../features/blog/components/Articles/ArticleList';
+import lastsectionimg from '../assets/images/blog-last-section.jpg';
 
 const Blog: FC = () => {
   return (
     <div>
       <section className="bg-plant_blog bg-cover bg-center">
-        <div className="bg-gradient-to-r via-dark-black via-30% from-dark-black
+        <div className="flex justify-center
+          bg-gradient-to-r via-dark-black via-30% from-dark-black
           xs:px-[32px] xs:pt-[40px] xs:pb-[50px]
           md:px-[50px] md:pt-[40px] md:pb-[50px]
           lg:px-[100px] lg:pt-[60px] lg:pb-[80px]"
         >
-          <div>
+          <div className="max-w-[1200px] w-full">
             <h1 className="text-white font-light 
               xs:text-3xl xs:font-normal
               md:text-[60px] md:font-light
@@ -25,7 +29,8 @@ const Blog: FC = () => {
               wonder.
             </div>
             <button 
-              className="mt-[40px] bg-blue text-white font-medium rounded-full duration-200 hover:bg-navy
+              className="mt-[40px] bg-blue text-white font-medium 
+              rounded-full duration-200 hover:bg-navy
               xs:py-[10px] xs:px-[37px]
               md:py-[15px] md:px-[45px]"
             >
@@ -34,9 +39,67 @@ const Blog: FC = () => {
           </div>
         </div>
       </section>
-      <div>
-
-      </div>
+      <section className="flex justify-center bg-gray-light
+        xs:px-[32px] xs:py-[30px]
+        md:px-[50px] md:py-[40px]
+        lg:px-[100px] lg:py-[60px]"
+      >
+        <div className="max-w-[1200px] w-full">
+          <TopicHighlightList />
+        </div>
+      </section>
+      <section className="flex flex-col items-center bg-blue-dark
+        xs:px-[32px] xs:py-[30px]
+        md:px-[50px] md:py-[40px]
+        lg:px-[100px] lg:py-[60px]"
+      >
+        <h1 className="text-3xl text-white">
+          Read & Watch
+        </h1>
+        <div className="mt-[10px] text-white font-light text-center">
+          Check back for new stories throughout the week.
+        </div>
+        <div className="mt-[50px] max-w-[1200px] w-full">
+          <ArticeList />
+        </div>
+      </section>
+      <section className="flex justify-center bg-white
+        xs:px-[32px] xs:py-[30px]
+        md:px-[50px] md:py-[40px]
+        lg:px-[100px] lg:py-[60px]"
+      >
+        <div className="flex xs:flex-col md:flex-row">
+          <div className="overflow-hidden 
+            xs:w-[100%] xs:h-[300px]
+            md:w-[60%] md:h-[350px]"
+          >
+            <img 
+              src={lastsectionimg} 
+              alt='last-section-image' 
+              className="object-cover w-full h-full"
+            />
+          </div>
+          <div className="bg-white
+            xs:w-[100%] xs:p-[20px] xs:flex xs:flex-col xs:items-center
+            md:w-[40%] md:p-[30px] md:block"
+          >
+            <div className="text-2xl text-dark">
+              Discover our blog
+            </div>
+            <div className="mt-[10px] text-dark font-light xs:text-center md:text-left">
+              Dive into plant profiles, gardening insights, and nature's 
+              interconnected tapestry. It's your green nook for botanical wonder.
+            </div>
+            <button className="mt-[20px] bg-blue text-white font-medium 
+              rounded-full duration-200 hover:bg-navy
+              xs:py-[10px] xs:px-[37px]
+              md:py-[15px] md:px-[45px]"
+            >
+              View all articles
+            </button>
+          </div>
+        </div>
+      </section>
     </div>
   )
 }
