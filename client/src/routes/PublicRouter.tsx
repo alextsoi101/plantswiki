e2f288048313a114730678056ab778fab95e7106
Alex Tsoi <alextsoi101@gmail.com>
2023-11-13 01:40:44 -0600

    Add BlogArticles.tsx page

--- client/src/routes/PublicRouter.tsx
+++ client/src/routes/PublicRouter.tsx
@@ -3,6 +3,7 @@ import Home from '../views/Home';
 import Plants from '../views/Plants';
 import PlantInfo from '../views/PlantInfo';
 import Blog from '../views/Blog';
+import BlogArticles from '../views/BlogArticles';
 
 const PublicRouter = () => {
 
@@ -12,6 +13,7 @@ const PublicRouter = () => {
       <Route path='/plants' element={<Plants />} />
       <Route path='/plants/:id' element={<PlantInfo />} />
       <Route path='/blog' element={<Blog />} />
+      <Route path='/blog/articles' element={<BlogArticles />} />
       <Route
         path='*'
         element={<Navigate to='/' replace />}
---
+++ client/src/views/BlogArticles.tsx
@@ -0,0 +1,62 @@
+import { FC, useState } from 'react';
+import ArticleList from '../features/blog/components/Articles/ArticleList';
+import TopicButtonsGroup, { Topic } from '../features/blog/components/Topics/TopicButtonsGroup';
+import TopicSelect from '../features/blog/components/Topics/TopicSelect';
+
+const BlogArticles: FC = () => {
+
+  const [topic, setTopic] = useState(Topic.all);
+
+  const handleChange = (topic: Topic): void => {
+    setTopic(topic)
+  }
+
+  return (
+    <div>
+      <section className="bg-white
+        xs:px-[32px] xs:pt-[20px] xs:pb-[50px]
+        md:px-[50px] md:pt-[20px] md:pb-[50px]
+        lg:px-[100px] lg:pt-[40px] lg:pb-[80px]"
+      >
+        <div>
+          <div className="flex flex-col justify-center items-center bg-white">
+            <div className="text-blue font-light xs:text-[40px] md:text-[50px]">
+              Read & Watch
+            </div>
+            <div className="mt-[15px] max-w-[600px] font-light text-center
+              xs:text-base md:xs:text-[18px]"
+            >
+              Go behind the scenes to get the latest in our cutting-edge science and 
+              uncover the fascinating world of plants and fungi.
+            </div>
+          </div>
+          <div className="xs:hidden
+            md:block md:pt-[30px] md:pb-[80px]"
+          >
+            <TopicButtonsGroup 
+              topic={topic}
+              handleChange={handleChange}
+            />
+          </div>
+          <div className="xs:pt-[30px] xs:pb-[30px]
+            md:hidden"
+          >
+            <div className="ml-[10px] text-dark font-medium">
+              Filter by category
+            </div>
+            <div className="mt-[6px]">
+              <TopicSelect 
+                onChange={handleChange}
+              />
+            </div>
+          </div>
+        </div>
+        <div>
+          <ArticleList />
+        </div>
+      </section>
+    </div>
+  )
+}
+
+export default BlogArticles;
