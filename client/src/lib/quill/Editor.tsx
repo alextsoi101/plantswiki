63825daa70ba912842cc78d3d377f5ae89918edb
Alex Tsoi <alextsoi101@gmail.com>
2023-11-15 20:35:04 -0600

    Edit quill lib configuration

--- client/src/lib/quill/Editor.tsx
+++ client/src/lib/quill/Editor.tsx
@@ -8,48 +8,16 @@ interface EditorProps {
   onChange: any;
 }
 
-const CustomSizeStyle = `
-  .ql-snow .ql-picker.ql-size .ql-picker-label[data-value="14px"]::before,
-  .ql-snow .ql-picker.ql-size .ql-picker-item[data-value="14px"]::before {
-    content: '14px';
-  }
-  .ql-snow .ql-picker.ql-size .ql-picker-label[data-value="16px"]::before,
-  .ql-snow .ql-picker.ql-size .ql-picker-item[data-value="16px"]::before {
-    content: '16px';
-  }
-  .ql-snow .ql-picker.ql-size .ql-picker-label[data-value="18px"]::before,
-  .ql-snow .ql-picker.ql-size .ql-picker-item[data-value="18px"]::before {
-    content: '18px';
-  }
-  .ql-snow .ql-picker.ql-size .ql-picker-label[data-value="20px"]::before,
-  .ql-snow .ql-picker.ql-size .ql-picker-item[data-value="20px"]::before {
-    content: '20px';
-  }
-  .ql-snow .ql-picker.ql-size .ql-picker-label[data-value="30px"]::before,
-  .ql-snow .ql-picker.ql-size .ql-picker-item[data-value="30px"]::before {
-    content: '30px';
-  }
-`;
-
-const styleSheet = document.createElement('style');
-styleSheet.type = 'text/css';
-styleSheet.innerText = CustomSizeStyle;
-document.head.appendChild(styleSheet);
-
-const Size = ReactQuill.Quill.import('attributors/style/size');
-Size.whitelist = ['14px', '16px', '18px', '20px', '30px'];
-ReactQuill.Quill.register(Size, true);
-
-
 const Editor: FC<EditorProps> = ({value, onChange}) => {
 
   const modules = {
     toolbar: [
-      [{ 'header': [1, 2, 3, false] }],
-      [{ 'size': ['14px', '16px', '18px', '20px', '30px'] }],
-      ['bold', 'italic', 'underline','strike', 'blockquote'],
-      [{'list': 'ordered'}, {'list': 'bullet'}, {'indent': '-1'}, {'indent': '+1'}],
-      ['link', 'image'],
+      [{ 'header': [1, false] }],
+      [{ 'size': ['Base'] }],
+      ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+      [{'list': 'bullet'}, {'indent': '-1'}, {'indent': '+1'}],
+      ['image'],
+      [{ 'align': ['justify', 'center', 'right'] }],
       ['clean']
     ],
   };
@@ -58,7 +26,7 @@ const Editor: FC<EditorProps> = ({value, onChange}) => {
     'header',
     'bold', 'italic', 'underline', 'strike', 'blockquote',
     'list', 'bullet', 'indent',
-    'link', 'image'
+    'image', 'align'
   ];
 
   return (
