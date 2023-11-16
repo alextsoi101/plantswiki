5af20b341df479fc0b71e417b1536809318856b2
Alex Tsoi <alextsoi101@gmail.com>
2023-11-15 20:45:22 -0600

    Edit Article page

--- client/src/views/Article.tsx
+++ client/src/views/Article.tsx
@@ -1,8 +1,11 @@
 import { FC } from 'react';
 import ArticleHeader from '../features/blog/components/Articles/ArticleHeader';
 import ArticleFooter from '../features/blog/components/Articles/ArticleFooter';
+import ArticleBody from '../features/blog/components/Articles/ArticleBody';
 
 const Article: FC = () => {
+
+  const articleHTML = '';
 
   return (
     <div>
@@ -10,13 +13,21 @@ const Article: FC = () => {
         <div>
           <ArticleHeader />
         </div>
-        <div>
-
+        <div className="flex justify-center
+          xs:px-[20px] xs:py-[40px]
+          md:px-[50px] md:py-[40px]
+          lg:px-[100px] lg:py-[60px]"
+        >
+          <div className="max-w-[970px] w-full">
+            <ArticleBody 
+              articleHTML={articleHTML}
+            />
+          </div>
         </div>
         <div className="bg-white
-          xs:px-[32px] xs:py-[20px]
+          xs:px-[32px] xs:py-[30px]
           md:px-[50px] md:py-[30px]
-          lg:px-[100px] lg:py-[30px]"
+          lg:px-[100px] lg:py-[40px]"
         >
           <ArticleFooter />
         </div>
