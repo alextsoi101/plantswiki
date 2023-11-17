ed069dac7f00b6e9322e14d968b3ad5626559105
Alex Tsoi <alextsoi101@gmail.com>
2023-11-17 16:41:41 -0600

    feat(user): add index.js for component exports

---
+++ client/src/features/user/index.ts
@@ -0,0 +1,7 @@
+export { default as AccountMenu } from './components/MyAccount/AccountMenu';
+export { default as AccountGeneral } from './components/MyAccount/AccountGeneral';
+export { default as AccountArticles } from './components/MyAccount/AccountArticles';
+export { default as AccountReactions } from './components/MyAccount/AccountReactions';
+export { default as AccountInformation } from './components/MyAccount/AccountInformation';
+export { default as AccountPassword } from './components/MyAccount/AccountPassword';
+export { AccountMenuContent } from './components/MyAccount/AccountMenu';
