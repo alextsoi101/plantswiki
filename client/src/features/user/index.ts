4e22d345b67f1c7c03459793b6452b1dec223fb5
Alex Tsoi <alextsoi101@gmail.com>
2023-11-18 23:22:34 -0600

    feat(user): update user feature imports file

--- client/src/features/user/index.ts
+++ client/src/features/user/index.ts
@@ -1,4 +1,5 @@
 export { default as AccountMenu } from './components/MyAccount/AccountMenu';
+export { default as AccountMenuMobile } from './components/MyAccount/AccountMenuMobile';
 export { default as AccountGeneral } from './components/MyAccount/AccountGeneral';
 export { default as AccountArticles } from './components/MyAccount/AccountArticles';
 export { default as AccountReactions } from './components/MyAccount/AccountReactions';
