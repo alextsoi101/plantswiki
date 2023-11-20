bde8e53e629796832995c49e767abeb6515039b2
Alex Tsoi <alextsoi101@gmail.com>
2023-11-19 19:46:16 -0600

    feat: add close on click outside  to modal layout

--- client/src/layouts/ModalLayout.tsx
+++ client/src/layouts/ModalLayout.tsx
@@ -1,16 +1,34 @@
-import { FC, ReactNode, ReactElement } from "react";
+import { FC, useEffect, useRef, ReactNode, ReactElement } from "react";
 
 interface ModalLayoutProps {
   children: ReactNode | ReactElement;
+  onClose: () => void;
 }
 
-const ModalLayout: FC<ModalLayoutProps> = ({children}) => {
-  document.body.style.overflow = 'hidden';
-  // document.body.style.overflow = 'unset';
+const ModalLayout: FC<ModalLayoutProps> = ({children, onClose}) => {
+
+  const modalRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const handleDocumentClick = (e: any) => {
+      if (modalRef.current !== e.target) return
+      onClose()
+    };
+  
+    document.addEventListener('click', handleDocumentClick);
+  
+    return () => {
+      document.removeEventListener('click', handleDocumentClick);
+    };
+  }, []);
 
   return (
-    <div className="fixed top-0 left-0 w-screen h-screen bg-dark/60 z-[10000]">
-      {children}
+    <div 
+      className="fixed top-0 left-0 w-screen h-screen bg-dark/60 z-[10000]"
+    >
+      <div ref={modalRef}>
+        {children}
+      </div>
     </div>
   )
 }
